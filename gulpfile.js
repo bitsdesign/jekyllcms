bbdf54a9bbcc8e6009f4e3dbb9fa8531270f9eb1
Shaun sandy <shaun7007@Shaun-Sandys-i-Mac.local>
2016-12-13 10:10:23 +0800

    first commit live

--- gulpfile.js
+++ gulpfile.js
@@ -39,22 +39,9 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     });
 });
 
-/**
- * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
- */
-// gulp.task('sass', function () {
-//     return gulp.src('_scss/main.scss')
-//         .pipe(sass({
-//             includePaths: ['scss'],
-//             onError: browserSync.notify
-//         }))
-//         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-//         .pipe(gulp.dest('_site/css'))
-//         .pipe(browserSync.reload({stream:true}))
-//         .pipe(gulp.dest('css'));
-// });
 /*
-Compass Sass task*/
+Compass Sass task
+*/
 
 gulp.task('sass', function() {
   browserSync.notify('<span style="color: grey">Running:</span> Sass compiling');
